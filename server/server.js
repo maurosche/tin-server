62d09feff0f49043217d6a46ce5ce0c4a6ad0227
Mauro Schettino <Schettino@CESVIARG>
2018-06-08 13:57:12 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,57 +1,57 @@
 require('./config/config');
 
-const path = require('path');
+//const path = require('path');
 
-const express = require('express');
+//const express = require('express');
 
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-const app = express();
-const bodyParser = require('body-parser');
+//const app = express();
+//const bodyParser = require('body-parser');
 
 // Add headers
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Website you wish to allow to connect
+//     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
  
-// parse application/json
-app.use(bodyParser.json())
+// // parse application/json
+// app.use(bodyParser.json())
 
-// habilitar la carpeta public
-app.use(express.static(path.resolve(__dirname, '../public')));
+// // habilitar la carpeta public
+// app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.use( require('./routes/index'));
+// app.use( require('./routes/index'));
 
-// ========================
-//   MONGOO BD
-// ========================
-mongoose.connect(process.env.urlDB, (err,res)=>{
-     if (err) 
-     {
-        console.log("ERROR EN BASE MONGOO : ",err);
-        throw err;
-     }
-     console.log("CONEXION MONGOO OK!");
-});
+// // ========================
+// //   MONGOO BD
+// // ========================
+// mongoose.connect(process.env.urlDB, (err,res)=>{
+//      if (err) 
+//      {
+//         console.log("ERROR EN BASE MONGOO : ",err);
+//         throw err;
+//      }
+//      console.log("CONEXION MONGOO OK!");
+// });
 
 // ========================
 //   REST API
