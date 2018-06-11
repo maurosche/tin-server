5815f6e9ddfb0ecfc28512c3f768b3839f7992a1
Mauro Schettino <Schettino@CESVIARG>
2018-06-11 11:35:25 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ const socketIO = require('socket.io');
 const app = express();
 
   // Add headers
-  server.use(function (req, res, next) {
+  app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
