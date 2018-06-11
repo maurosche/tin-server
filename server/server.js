1111b6e1ff099f58c8bdd08485a97e4d6ff86b87
Mauro Schettino <Schettino@CESVIARG>
2018-06-11 16:48:51 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -71,8 +71,11 @@ const http = require('http');
 const appSocket = express();
 const socketIO = require('socket.io');
 let server = http.createServer(app);
-module.exports.io = socketIO(server);
-require('./providers/socket.provider');
+//module.exports.io = socketIO(server);
+//require('./providers/socket.provider');
+let io = socketIO(server);
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 server.listen(process.env.PORT, (err) => {
 
