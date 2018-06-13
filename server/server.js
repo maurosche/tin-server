f96554aa0337f54abe96697d602fcc3344c12864
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 12:34:08 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ socket.on('connection', function(connection) {
 
    console.log('User Connected');
    
-   connection.on('message', function(msg){
-     socket.emit('message', msg);
-   });
+//    connection.on('message', function(msg){
+//      socket.emit('message', msg);
+//    });
 
 });
 
