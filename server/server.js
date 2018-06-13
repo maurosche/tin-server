5148fd00f739dc9afa97337dc570141fd250b176
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 12:23:44 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,25 +1,45 @@
-const express = require('express');
-const socketIO = require('socket.io')
-const http = require('http');
-
-const path = require('path');
-
-const app = express();
-let server = http.Server(app);//http.createServer(app);
-
-const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
-
-app.use(express.static(publicPath));
-
-// IO = esta es la comunicacion del backend
-module.exports.io = socketIO(server);
-require('./providers/socket.provider');
-
-server.listen(port, (err) => {
-
-    if (err) throw new Error(err);
-
-    console.log(`Servidor corriendo en puerto ${ port }`);
-
+var socket = require('socket.io'), http = require('http'),
+  server = http.createServer(), socket = socket.listen(server);
+socket.on('connection', function(connection) {
+   console.log('User Connected');
+   
+   connection.on('message', function(msg){
+     socket.emit('message', msg);
+   });
 });
+server.listen(3000, function(){
+console.log('Server started');
+});
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const socketIO = require('socket.io')
+// const http = require('http');
+
+// const path = require('path');
+
+// const app = express();
+// let server = http.createServer(app);
+
+// const publicPath = path.resolve(__dirname, '../public');
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(publicPath));
+
+// // IO = esta es la comunicacion del backend
+// module.exports.io = socketIO(server);
+// require('./providers/socket.provider');
+
+// server.listen(port, (err) => {
+
+//     if (err) throw new Error(err);
+
+//     console.log(`Servidor corriendo en puerto ${ port }`);
+
+// });
