39eb6567c5a22db4753c5c6007603065018bbde0
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 16:54:50 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,28 @@ const mongoose = require('mongoose');
 const publicPath = path.resolve(__dirname, '../public');
 const bodyParser = require('body-parser');
 
-let server = http.createServer(app);
+//Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+//let server = http.createServer(app);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
