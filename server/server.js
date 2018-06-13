cb389b0b3a904b7b9ca83ba571b13a1ca3be1667
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 15:06:47 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,47 +1,25 @@
-require('./config/config');
-
 const express = require('express');
 const socketIO = require('socket.io')
 const http = require('http');
-const path = require('path');
-const app = express();
-const mongoose = require('mongoose');
 
+const path = require('path');
+
+const app = express();
 let server = http.createServer(app);
 
-const bodyParser = require('body-parser');
+//const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
-
-// habilitar la carpeta public
-app.use(express.static(path.resolve(__dirname, '../public')));
-
-app.use( require('./routes/index'));
-
-// ========================
-//   MONGOO BD
-// ========================
-mongoose.connect(process.env.urlDB, (err,res)=>{
-     if (err) 
-     {
-        console.log("ERROR EN BASE MONGOO : ",err);
-        throw err;
-     }
-     console.log("CONEXION MONGOO OK!");
-});
+app.use(express.static(publicPath));
 
 // IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
 require('./providers/socket.provider');
 
-server.listen(process.env.PORT, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+    console.log(`Servidor corriendo en puerto ${ port }`);
 
 });
