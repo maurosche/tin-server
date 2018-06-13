1c198fbd4abf11eba08c713f8f3425d0ee42d6ee
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 12:29:57 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 var socket = require('socket.io'), http = require('http'),
   server = http.createServer(), socket = socket.listen(server);
+
+
 socket.on('connection', function(connection) {
+
    console.log('User Connected');
    
    connection.on('message', function(msg){
      socket.emit('message', msg);
    });
+
 });
+
+setInterval(() => socket.emit('message', new Date().toTimeString()), 1000);
+
 server.listen(3000, function(){
 console.log('Server started');
 });
