5fe2a4d8b3dc87f768600ce86ef17e7ab45e59eb
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 10:35:51 -0300

    commit

--- server/providers/socket.provider.js
+++ server/providers/socket.provider.js
@@ -1,6 +1,6 @@
 const {io} = require('../server');
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval((io) => io.emit('time', new Date().toTimeString()), 1000);
 
 // io.on('connection', (client)=>{
 // });
