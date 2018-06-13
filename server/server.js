8bc3971bba7d214bafe3299d7fab187891956ac3
Mauro Schettino <Schettino@CESVIARG>
2018-06-13 15:09:49 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,25 +1,37 @@
+require('./config/config');
+
 const express = require('express');
 const socketIO = require('socket.io')
 const http = require('http');
-
 const path = require('path');
-
 const app = express();
-let server = http.createServer(app);
+const mongoose = require('mongoose');
 
+let server = http.createServer(app);
 const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+
+// ========================
+//   MONGOO BD
+// ========================
+mongoose.connect(process.env.urlDB, (err,res)=>{
+    if (err) 
+    {
+       console.log("ERROR EN BASE MONGOO : ",err);
+       throw err;
+    }
+    console.log("CONEXION MONGOO OK!");
+});
 
 // IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
 require('./providers/socket.provider');
 
-server.listen(port, (err) => {
+server.listen(process.env.PORT, (err) => {
 
     if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto ${ port }`);
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 
 });
