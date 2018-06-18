de1e8e9a371d4d94477ac2bfc3929fc6c26d27cb
Mauro Schettino <Schettino@CESVIARG>
2018-06-18 15:44:59 -0300

    commit

--- server/providers/chat.provider.js
+++ server/providers/chat.provider.js
@@ -36,7 +36,7 @@ let  getChats = (idUsuario,callback,callbackError)=> {
                           if: { $eq: [ "5b1991bf1a102b00147b9aae", "#_id.usuarioEmisor" ] },
                           then: "$$REMOVE",
                           else: "$_id.usuarioEmisor"
-                       }
+                       } 
                     }
                  }
             },        
