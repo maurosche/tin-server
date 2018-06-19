52d47a8167af2ab28f2fdc442346a30b977f4292
Mauro Schettino <Schettino@CESVIARG>
2018-06-19 11:02:21 -0300

    commit

--- server/providers/chat.provider.js
+++ server/providers/chat.provider.js
@@ -136,9 +136,10 @@ let  vistoChat = (idUsuarioEmisor,idUsuarioReceptor,callback,callbackError)=> {
     console.log('VISTO RECEPTOR', idUsuarioReceptor);
 
     Chat.updateMany(
-     { idUsuarioEmisor : new ObjectId(idUsuarioEmisor), idUsuarioReceptor : new ObjectId(idUsuarioReceptor)} ,
+     { idUsuarioEmisor : new ObjectId(idUsuarioEmisor), 
+       idUsuarioReceptor : new ObjectId(idUsuarioReceptor)} ,
      { $set: { "visto" : true } },
-         callback()
+         callback(data)
      );  
 };
 
