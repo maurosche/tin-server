646a79996f92ee81333d589ccf80951ef3e57a04
Mauro Schettino <Schettino@CESVIARG>
2018-06-25 11:59:51 -0300

    commit

--- server/providers/foto.provider.js
+++ server/providers/foto.provider.js
@@ -16,28 +16,28 @@ let  postFotosPerfil = (idUsuario,fotosList,callback,callbackError)=> {
     let fotosListEnDisco = new Array();
 
     console.log('================= fotosList.length : ',  fotosList.length);
-    
- 
-    for(var i = 0; i > fotosList.length;i++){
-        
-        let extension = '.jpeg'; 
-        let nombreArchivo = `${new Date().getMilliseconds()}.${extension}`;  
-        let pathImagen = path.resolve(__dirname, `../../uploads/perfil/` + idUsuario + `/${ nombreArchivo }`);
-        
-        console.log('=================pathImagen : ', pathImagen);
 
-        fs.appendFile(pathImagen, fotosList[i].src, function (err) {
-            if (err) 
-            {
-                return callbackError(err);
-            }
-            else
-            {
-                fotosListEnDisco.push(pathImagen);
-                console.log('GUARDADO!');
-            }            
-        });
-    }  
+ 
+    // for(var i = 0; i > fotosList.length;i++){
+        
+    //     let extension = '.jpeg'; 
+    //     let nombreArchivo = `${new Date().getMilliseconds()}.${extension}`;  
+    //     let pathImagen = path.resolve(__dirname, `../../uploads/perfil/` + idUsuario + `/${ nombreArchivo }`);
+        
+    //     console.log('=================pathImagen : ', pathImagen);
+
+    //     fs.appendFile(pathImagen, fotosList[i].src, function (err) {
+    //         if (err) 
+    //         {
+    //             return callbackError(err);
+    //         }
+    //         else
+    //         {
+    //             fotosListEnDisco.push(pathImagen);
+    //             console.log('GUARDADO!');
+    //         }            
+    //     });
+    // }  
 
     callback(fotosListEnDisco);
 
