2199b0181574b65c713e42b4be11b81591273b6d
Mauro Schettino <Schettino@CESVIARG>
2018-06-25 16:35:35 -0300

    commit

--- server/providers/foto.provider.js
+++ server/providers/foto.provider.js
@@ -22,7 +22,7 @@ let  postFotosPerfil = (idUsuario,fotosList,callback,callbackError)=> {
     for(var i = 0; i < fotosList.length;i++){
         
         let extension = fotosList[i].indexOf('jpeg') < 0 ? 'png' : 'jpeg';      
-        let nombreArchivo = `${Math.random()}-${new Date().getMilliseconds()}.${extension}`;  
+        let nombreArchivo = `${new Date().getMilliseconds()}.${extension}`;  
         let pathImagen = path.resolve(__dirname, `../../uploads/perfil/` + idUsuario + `/${ nombreArchivo }`);
         //let pathImagen = path.resolve(__dirname, `../../uploads/${ nombreArchivo }`);
         var base64Data = fotosList[i].replace(/^data:image\/png;base64,/, "");
