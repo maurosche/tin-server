5d26b6034b78d1680a3ca035db05857a6e943ded
Mauro Schettino <Schettino@CESVIARG>
2018-06-27 15:21:37 -0300

    commit

--- server/providers/foto.provider.js
+++ server/providers/foto.provider.js
@@ -27,34 +27,35 @@ let  getFotosPerfil = (usuario,callback,callbackError)=> {
 
                 let archivo = process.env.urlFotos + 'fotoPerfil/' + usuario._id + '/' +  element;
 
-                list.push(archivo);
+                //list.push(archivo);
 
-                    // //read image file
-                    // fs.readFile( archivo, (err, data)=>{
+                    //read image file
+                    fs.readFile( archivo, (err, data)=>{
                         
-                    //     //error handle
-                    //     if(err) console.log('ERROR : ', err);
+                        //error handle
+                        if(err) console.log('ERROR : ', err);
                         
-                    //     //get image file extension name
-                    //     let extensionName = path.extname(archivo);
+                        //get image file extension name
+                        let extensionName = path.extname(archivo);
                         
-                    //     //convert image file to base64-encoded string
-                    //     let base64Image = new Buffer(data, 'binary').toString('base64');
+                        //convert image file to base64-encoded string
+                        let base64Image = new Buffer(data, 'binary').toString('base64');
                         
-                    //     //combine all strings
-                    //     let imgSrcString = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;                        
+                        //combine all strings
+                        let imgSrcString = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;                        
 
-                    //     //send image src string into jade compiler
-                    //     list.push( imgSrcString );
+                        //send image src string into jade compiler
+                        list.push( imgSrcString );
 
-                    //     if(dir.length == list.length){
-                    //         return callback({idUsuario , fotos : list});
-                    //     }
-                    // })
+                        if(dir.length == list.length){
+                            usuario.fotos = list;
+                            return callback({usuario});
+                        }
+                    })
             });
 
-            usuario.fotos = list;
-            return callback({usuario});
+            // usuario.fotos = list;
+            // return callback({usuario});
         });
          
 };
