6947156dbde6868fde260b23c490b753da005e69
Mauro Schettino <Schettino@CESVIARG>
2018-06-27 10:24:26 -0300

    commit

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -27,7 +27,7 @@ let callbackError = (data,res)=>{
 app.get('/usuario', verificarTokenAdmin, function(req,res){
 
     let idUsuario = req.query.idUsuario || 0;
-    let list = new Array();
+
     //Traigo usuarios a los que no le haya dado like
     getLikesPropios(idUsuario,(likes)=>{
 
@@ -45,19 +45,13 @@ app.get('/usuario', verificarTokenAdmin, function(req,res){
 
             getFotosPerfil(result[index]._id,(fotos)=>{
 
-                let user = result[index];
-                user['fotos'] = fotos;
+                result[index].fotos = fotos;
 
                 console.log("FOTOSSSSSSSSSSSSSSSSSSSSS result[index] : ", result[index]);
-                console.log("FOTOSSSSSSSSSSSSSSSSSSSSS user : ", user);
-                console.log("FOTOSSSSSSSSSSSSSSSSSSSSS fotos : ", fotos);
-                console.log("FOTOSSSSSSSSSSSSSSSSSSSSS user['fotos']  : ", user['fotos'] );
-                list.push(user);
 
                 if (index+1 == result.length) {
-                    res.json({ok:true,result : list });
+                    res.json({ok:true,result  });
                 }
-
 
             },(data)=>{callbackError(data,res)});    
         }
