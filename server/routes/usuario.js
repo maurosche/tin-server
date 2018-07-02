aa21d903fb56d1aa2a6b474a359304adc84bfafe
Mauro Schettino <Schettino@CESVIARG>
2018-07-02 15:02:52 -0300

    commit

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -36,6 +36,8 @@ app.get('/usuario', verificarTokenAdmin, function(req,res){
 
         let ids = new Array();
 
+        console.log("LIKESSSSSSSSSSSSSSSSSssss :",likes);
+
         likes.forEach(element => {
             ids.push( element.usuarioReceptor._id);
         });
